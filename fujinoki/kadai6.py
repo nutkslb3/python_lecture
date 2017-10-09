1ff5c989a3f66a2dc1c6d363136d2f600d06baaf
TaroFujinoki <tarofujinoki1115@gmail.com>
2017-10-10 02:21:07 +0900

    課題が完了しました

---
+++ fujinoki/kadai6.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+def main():
+    str = "I am a PERFECT HUMAN"
+    print  str 
+    print  str.split(" ")
+
+    
+
+if __name__ == '__main__':
+    main()
