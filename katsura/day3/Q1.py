bcbe3a12beae46f4191de7be832e388f0fc56b65
katura <s151024@stn.nagaokaut.ac.jp>
2017-10-10 03:36:59 +0900

    Q1 desu

--- katsura/day3/Q1.py
+++ katsura/day3/Q1.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-def main():
-    print('a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-           a = [1:10:2]')
-if __name__ == '__main__':
-    main()
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = a[1:10:2]
+print(a)
